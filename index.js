af07e249029f71c8f89fbc171ba4e3612b90aee4
Andres Cardenas <andrescardenase77@gmail.com>
2026-06-04 20:47:51 -0500

    feat: exclusive repository for supply get uris

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { createClient } = require("@supabase/supabase-js");
+const supplyRouter = require("./routes/supplyRoutes");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3004;
+
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+);
+
+app.set("supabaseClient", supabase);
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.static("./views"));
+
+app.use(express.json());
+app.use("/", supplyRouter);
+
+app.listen(port, () => console.log("Server running on port " + port));
