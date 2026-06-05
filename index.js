48221853d890f27543a3d5c9645099ef4c0470cf
victoriadiazcaizaluisa <cvdiaz3@espe.edu.ec>
2026-06-04 21:35:44 -0500

    patient uris

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { createClient } = require("@supabase/supabase-js");
 const supplyRouter = require("./routes/supplyRoutes");
+const patientRouter = require("./routes/patientRoutes");
 require("dotenv").config();
 
 const app = express();
@@ -19,5 +20,6 @@ app.use(express.static("./views"));
 
 app.use(express.json());
 app.use("/", supplyRouter);
+app.use("/", patientRouter);
 
 app.listen(port, () => console.log("Server running on port " + port));
